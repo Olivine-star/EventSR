82f7fe03bdb8f595b982a3f3b9a186c0620eebb8
Olivine-star <b2776410593@gmail.com>
2025-06-09 01:39:42 +1000

    第一个版本

---
+++ nMnist/train.bat
@@ -0,0 +1,18 @@
+@echo off
+REM
+call conda activate EventSRSNN
+
+REM
+cd /d D:\PycharmProjects\EventSR\SR-ES1\EventStream-SR-main\nMnist
+
+REM
+python trainNmnist.py ^
+--bs 64 ^
+--savepath "./ckpt/" ^
+--epoch 30 ^
+--showFreq 50 ^
+--lr 0.1 ^
+--cuda "0" ^
+--j 4
+
+pause
